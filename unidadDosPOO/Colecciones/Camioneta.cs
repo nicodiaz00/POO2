c698fbd1a7b0b35e2704b54819cb39436e6ad8b0
Nico <ngdiaz00@gmail.com>
2023-12-07 12:12:34 -0300

    actualizando practica

---
+++ unidadDosPOO/Colecciones/Camioneta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    internal class Camioneta
+    {
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+
+        public List<Camioneta> listaCamioneta(int valor)
+        {
+            List<Camioneta> listasCamionetas =new List<Camioneta>();
+            for(int i = 0; i < valor; i++)
+            {
+                Console.WriteLine("Comience a ingresar camionetas");
+            }
+           
+        }
+    }
+
+    
+}
---
+++ unidadDosPOO/ejemplo1/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Persona
+    {
+        private string nombre;
+        
+        private string apellido;
+        private int legajo;
+    }
+}
